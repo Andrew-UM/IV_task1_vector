b628ef62a686081a6bd656b7c3d5d3ac266e89a9
Andrew-UM <andeyuvarov@gmail.com>
2023-03-06 02:21:01 +0300

    done?

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 #include "Vector.cpp"
 #include <iostream>
 
-int main() {
+int main()
+{
 
-    Vector<int, 5> A;
+	Vector<int, 5> A;
 
-    A.push_back(0);
-    A.push_back(1);
-    A.push_back(2);
-    A.push_back(3);
-    A.push_back(4);
-    A.push_back(5);
-    A.push_back(6);
-    A.push_back(7);
-    A.push_back(8);
-    A.push_back(9);
-    A.push_back(10);
-    A.push_back(11);
-    A.push_back(12);
+	A.push_back(0);
+	A.push_back(1);
+	A.push_back(2);
+	A.push_back(3);
+	A.push_back(4);
+	A.push_back(5);
+	A.push_back(6);
+	A.push_back(7);
+	A.push_back(8);
+	A.push_back(9);
+	A.push_back(10);
+	A.push_back(11);
+	A.push_back(12);
 
-    A.pop_back();
+	A.pop_back();
 
-    for (auto x: A) {
-        std::cout << x << std::endl;
-    }
+	for (auto x: A)
+	{
+		std::cout << x << std::endl;
+	}
 }
